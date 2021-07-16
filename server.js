7e5028de31a2c42be5bd5efc9dc31423bcb4137a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-15 22:35:34 -0700

    add comments

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
+// import dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// import models
 const db = require("./models");
 
+// create the Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// set up the Express app to generate request logs
 app.use(logger("dev"));
 
+// set up the Express app to parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// set up the Express app to serve static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
+// set up the Express app to use routes
 app.use(require("./routes/api.js"));
 
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
