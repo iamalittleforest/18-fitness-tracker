7c1d0bbb5a906cc1d6e57f94bc7324e72b7ba167
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-17 20:52:35 -0700

    outline structure of api routes

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+var db = require("../models");
+
+module.exports = (app) => {
+
+  // get all workouts (for last workout)
+  
+  // put workout (by adding exercise)
+
+  // post new workout
+
+  // get all workouts in range
+
+};
