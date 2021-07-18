34f1233fd61c621587ae0ef59106af0f4f0e9518
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-07-17 21:36:41 -0700

    define api route for all workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,30 @@
+const router = require("express").Router();
 var db = require("../models");
 
-module.exports = (app) => {
+// get all workouts (for last workout)
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
+})
 
-  // get all workouts (for last workout)
-  
-  // put workout (by adding exercise)
+// put workout (by adding exercise)
+router.put("/api/workouts/:id", (req, res) => {
 
-  // post new workout
+})
 
-  // get all workouts in range
+// post new workout
+router.post("/api/workouts", ({ body }, res) => {
 
-};
+})
+
+// get all workouts in range
+router.get("/api/workouts", (req, res) => {
+
+})
+
+module.exports = router;
